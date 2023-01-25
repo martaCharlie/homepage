ef75c705a42fb7e89c67bc7ff371eea6741668d7
Marta Wojcik <marta.bus@onet.pl>
2023-01-25 16:39:35 +0100

    js function howMuchTime moved up

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@
     }
     welcome();
 
+    const howMuchTime = () => {
+        const day = document.querySelector(".day").innerText;
+        const howMuch = document.querySelector(".howMuch");
+        if (day === "poniedziałek" || day === "wtorek") {
+            howMuch.innerText = "dużo "
+        } else if (day === "środa" || day === "czwartek") {
+            howMuch.innerText = "trochę "
+        } else {
+            howMuch.innerText = "mało "
+        }
+    }
+    
+    howMuchTime();
+
     const toggleBackground = () => {
         const body = document.querySelector(".body");
         const themeName = document.querySelector(".themeName");
@@ -23,16 +37,3 @@
     init();
 }
 
-const init = () => {
-    const day = document.querySelector(".day").innerText;
-    const howMuch = document.querySelector(".howMuch");
-    if (day === "poniedziałek" || day === "wtorek") {
-        howMuch.innerText = "dużo "
-    } else if (day === "środa" || day === "czwartek") {
-        howMuch.innerText = "trochę "
-    } else {
-        howMuch.innerText = "mało "
-    }
-}
-
-init();
